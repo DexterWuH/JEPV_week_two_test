b308e970cd27706c65c421828c3a3234de86257a
EPedroza21 <enrique.pedroza92@gmail.com>
2023-11-10 09:21:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jepv_week_two_test/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jepv_week_two_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
